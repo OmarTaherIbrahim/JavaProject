2cbb4779f302ed8673b9b6e5edc8bab138a67757
Ward Hindawi <whindawi13@gmail.com>
2019-01-04 20:41:01 +0200

    test selection menu update

--- src/data/UserInfo.java
+++
@@ -1,7 +0,0 @@
-package data;
-
-public class UserInfo {
-	public static String UserName="Unkown";
-	public static String Password="Unkown";
-	
-}
--- src/data/utils/ScoreHandler.java
+++
@@ -1,5 +0,0 @@
-package data.utils;
-
-public class ScoreHandler {
-
-}
