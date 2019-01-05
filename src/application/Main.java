50e57d83a50c7ec1cd5b1fdfb2fedf23fa084d32
Kaliham <Kaliham@users.noreply.github.com>
2019-01-05 23:40:52 +0200

    omar edits

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			stage = primaryStage;
-			Parent root = FXMLLoader.load(getClass().getResource("../ui/FXML/multiTest.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("../ui/FXML/ImageTest.fxml"));
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("");
--- src/ui/controller/ImageTestController.java
+++ src/ui/controller/ImageTestController.java
@@ -59,9 +59,9 @@ public class ImageTestController implements Initializable {
     @Override
 	public void initialize(URL location, ResourceBundle resources) {
 		randomNumbers= new ArrayList<Integer>();
-    	lessonlist = new ColorsList();
+    	lessonlist = new AlphabetsList();
     	answers=new ArrayList<Integer>() ;
-    	num_of_questions.setText(result + "");
+    	num_of_questions.setText(0 + "");
     	fillRandomNumbers();
     	setQuestion(currIndex);
 	}
@@ -122,6 +122,7 @@ public class ImageTestController implements Initializable {
     	generateAnswers();
     	fillButtons();
     }
+    
     private void goToResultScreen() {
       	Parent p;
     		try {
@@ -138,7 +139,26 @@ public class ImageTestController implements Initializable {
     			e.printStackTrace();
         	
         }
+    }
+    @FXML
+    void goback(ActionEvent event) {
+	   Parent p;
+		try {
+			p = FXMLLoader.load(getClass().getResource("../FXML/MainMenu.fxml"));
+		
+			Scene profileSceen = new Scene(p);	
+			Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			stage.setScene(profileSceen);
+			stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
 
-}
+    @FXML
+    void gobackTab(ActionEvent event) {
+
+    }
     
 }
