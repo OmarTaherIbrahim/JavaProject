b61d015fdc607c59b6f66a25b31d7d9b8fcf353b
Ward Hindawi <whindawi13@gmail.com>
2019-01-05 20:59:10 +0200

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,9 +4,9 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import utils.server.ServerManager;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import utils.server.ServerManager;
 
 
 public class Main extends Application {
@@ -15,15 +15,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			stage = primaryStage;
-<<<<<<< HEAD
 
-
-			Parent root = FXMLLoader.load(getClass().getResource("../ui/FXML/PlusMinusTest.fxml"));
-=======
 			ServerManager severManager = new ServerManager();
 			severManager.sendString();
 			Parent root = FXMLLoader.load(getClass().getResource("../ui/FXML/Learn.fxml"));
->>>>>>> 4c2a52e193d6dc28d5f116e9f181cd823d152444
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("");
