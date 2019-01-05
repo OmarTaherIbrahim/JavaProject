92681edfd31f96cf2c2fb7ab88f12ed758caaa31
Samer Al-Omari <sameromari98@outlook.com>
2019-01-05 23:43:21 +0200

    update

--- src/ui/controller/TestMenuController.java
+++ src/ui/controller/TestMenuController.java
@@ -1,6 +1,7 @@
 
 package ui.controller;
 
+import java.awt.Event;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -8,6 +9,8 @@ import java.util.ResourceBundle;
 import com.jfoenix.controls.JFXButton;
 
 import data.lists.AlphabetsList;
+import data.lists.ColorsList;
+import data.lists.ShapesList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,11 +22,15 @@ import javafx.scene.control.Tab;
 import javafx.stage.Stage;
 
 public class TestMenuController implements Initializable {
-	 @FXML
-	    private JFXButton wordBTN;
 
-	    @FXML
-	    private JFXButton imageBTN;
+	 @FXML
+	 private JFXButton imageTest2;
+
+	 @FXML
+	 private JFXButton imageTest3;
+
+	 @FXML
+	 private JFXButton imageTest1;
 
 	
 	@FXML
@@ -56,7 +63,8 @@ public class TestMenuController implements Initializable {
     }
     
     @FXML
-    void imageTest(ActionEvent event) {
+    void imageTestABC(ActionEvent event) {
+    	
     	Parent p;
 		try {
 			FXMLLoader L = new FXMLLoader(getClass().getResource("../FXML/ImageTest.fxml"));
@@ -71,41 +79,20 @@ public class TestMenuController implements Initializable {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	
 
     }
 
     @FXML
-    void wordTest(ActionEvent event) {
-    	
-     	Parent p;
-    		try {
-    			FXMLLoader L = new FXMLLoader(getClass().getResource("../FXML/WordTest.fxml"));
-    			p=L.load();
-    			WordTestController controller=L.<WordTestController>getController();
-    			controller.
-    			Scene profileSceen = new Scene(p);	
-    			Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-    			stage.setScene(profileSceen);
-    			stage.show();
-    		} catch (IOException e) {
-    			// TODO Auto-generated catch block
-    			e.printStackTrace();
-    		}
-
-	}
-	
-    @FXML
-    void imageTest(ActionEvent event) {
+    void imageTestColor(ActionEvent event) {
     	
     	Parent p;
 		try {
 			FXMLLoader L = new FXMLLoader(getClass().getResource("../FXML/ImageTest.fxml"));
 			p=L.load();
 			ImageTestController controller=L.<ImageTestController>getController();
-			controller.setLessonList(new AlphabetsList());
+			controller.setLessonList(new ColorsList());
 			Scene profileSceen = new Scene(p);	
-			Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+			Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 			stage.setScene(profileSceen);
 			stage.show();
 		} catch (IOException e) {
@@ -116,22 +103,21 @@ public class TestMenuController implements Initializable {
     }
 
     @FXML
-    void wordTest(ActionEvent event) {
-       	Parent p;
-    		try {
-    			FXMLLoader L = new FXMLLoader(getClass().getResource("../FXML/WordTest.fxml"));
-    			p=L.load();
-    			ImageTestController controller=L.<ImageTestController>getController();
-    			controller.setLessonList(new AlphabetsList());
-    			Scene profileSceen = new Scene(p);	
-    			Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
-    			stage.setScene(profileSceen);
-    			stage.show();
-    		} catch (IOException e) {
-    			// TODO Auto-generated catch block
-    			e.printStackTrace();
-    		}
-
+    void imageTestShape(ActionEvent event) {
+    	Parent p;
+		try {
+			FXMLLoader L = new FXMLLoader(getClass().getResource("../FXML/ImageTest.fxml"));
+			p=L.load();
+			ImageTestController controller=L.<ImageTestController>getController();
+			controller.setLessonList(new ShapesList());
+			Scene profileSceen = new Scene(p);	
+			Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			stage.setScene(profileSceen);
+			stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
     }
 
